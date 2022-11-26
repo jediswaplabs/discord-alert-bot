53ef669f6087dbc987fc3adf5fc3625b2f237ef2
Al Matty <al@almatty.com>
2022-11-25 20:05:57 -0800

    Updated TODO's

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ disc_bot.run_bot()
 
 # TODO: add botpic, about info, description
 # TODO: implement Telegram frontend (button menu for data entry)
-# TODO: function for users to delete their data!
-# TODO: Prompt for discord handle, guild/channel to listen to
+# DONE: function for users to delete their data!
+# DONE: Prompt for discord handle, guild/channel to listen to
 # TODO: Move data from local json to cloud (MongoDB)
 # TODO: Switch off logging before bot is 'released'
 # TODO: Implement some way to assert that bot is not offline
