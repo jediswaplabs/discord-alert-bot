109355527ea18808cb44898cd52b75725b76cab0
Al Matty <al@almatty.com>
2023-02-11 13:12:26 +0100

    Forbidden error is handled now. Users who blocked the bot will now be deleted from the database at the first Forbidden error (closes #55 ).

--- helpers.py
+++ helpers.py
@@ -3,6 +3,7 @@ This file contains contains helper functions callable by any of the 2 bots.
 '''
 
 import logging
+import pickle
 
 
 def log(msg, level="INFO") -> None:
@@ -33,8 +34,13 @@ def iter_to_str(iterable, ignore_list=[], prefix="\n\n", suffix="\n\n") -> str:
     """Parses iterable to string, one entry per line."""
     if iterable == None:
         return ""
-    
+
     iterable = [str(x) for x in iterable if x not in ignore_list]
     contents = "\n".join(iterable)
 
     return str(prefix+contents+suffix)
+
+
+def write_to_pickle(obj, filepath):
+    with open(filepath, 'wb') as handle:
+        pickle.dump(obj, handle, protocol=pickle.HIGHEST_PROTOCOL)
